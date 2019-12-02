2c92e5c95a2e737be9a1a6c2c90bc5bf09bab07c
Philipp Hauber <44675474+philippotto-hauber@users.noreply.github.com>
2019-12-02 12:13:04 +0100

    load data from FRED

--- codes.R
+++ codes.R
@@ -1,1 +1,19 @@
+rm(list = ls())
+options(scipen = 999)
 
+# libraries
+library(fredr)
+fredr_set_key # set API key
+library(purrr)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+
+# load yield curve data and recession indicator from FRED #################
+data_fred <-map_dfr(c("USREC", "GS10", "TB3MS"), fredr)
+
+# filter sample
+data_fred <- data_fred %>% spread(series_id, value) %>% filter(date >= "1950-01-01") %>% mutate(spread = GS10 - TB3MS)
+
+ ggplot(data_fred, aes(date, spread, ))+
+     geom_line()
